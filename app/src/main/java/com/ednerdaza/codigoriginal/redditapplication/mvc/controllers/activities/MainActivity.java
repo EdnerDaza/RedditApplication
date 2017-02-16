d7de489ef9bd8b5feac75d31effcdc61c88fa296
Edner Enrique Daza Miranda <edner.daza@gmail.com>
2017-02-16 11:48:24 -0500

    splashactivity y nuevas configuraciones

--- app/src/main/java/com/ednerdaza/codigoriginal/redditapplication/mvc/controllers/activities/MainActivity.java
+++ app/src/main/java/com/ednerdaza/codigoriginal/redditapplication/mvc/controllers/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ednerdaza.codigoriginal.redditapplication;
+package com.ednerdaza.codigoriginal.redditapplication.mvc.controllers.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.ednerdaza.codigoriginal.redditapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/ednerdaza/codigoriginal/redditapplication/mvc/controllers/fragments/MainActivityFragment.java
+++ app/src/main/java/com/ednerdaza/codigoriginal/redditapplication/mvc/controllers/fragments/MainActivityFragment.java
@@ -1,10 +1,12 @@
-package com.ednerdaza.codigoriginal.redditapplication;
+package com.ednerdaza.codigoriginal.redditapplication.mvc.controllers.fragments;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.ednerdaza.codigoriginal.redditapplication.R;
 
 /**
  * A placeholder fragment containing a simple view.
